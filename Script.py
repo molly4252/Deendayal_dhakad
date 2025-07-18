c3fe0196d8adcbd037df26faea7047681bf5b9c5
molly4252 <mollydeanx1@gmail.com>
2025-07-18 09:43:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -271,7 +271,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamcinezone">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/dreamcinezone>[Dreamcine_Botz]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/+MPYqWUWHJcY2OTVl">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/+MPYqWUWHJcY2OTVl>[dragon2025moviebot]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
